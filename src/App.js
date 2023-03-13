29f17b496ad6e51a73cbd318711ef2819729fd21
dkrol24 <krold2191@gmail.com>
2023-03-13 19:41:32 +0100

    .

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chcesz poznać kogoś i umówić się na randkę?</h1>
+      <h1>Zobacz strony na których osoby czekają właśnie na Ciebie!</h1>
+      <h1>
+        Najedź myszką na jedną z nich i przeczytaj czy pasuje do Twoich upodobań
+      </h1>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          width: "100%",
+          height: "45vh",
+          background: "pink",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{ background: "black", width: "100%", height: "1px" }}
+        ></div>
+        <div
+          style={{ background: "black", width: "100%", height: "1px" }}
+        ></div>
+        <div
+          style={{ background: "black", width: "100%", height: "1px" }}
+        ></div>
+        <a href="https://redirecting3.eu/p/C4kJ/OKWX/UNG0" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              Flirtwedwoje
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>ads1</span>
+              <br />
+            </a>
+          </div>
+        </a>
+        <a href="https://redirecting3.eu/p/C4kJ/GmZ7/5SsT" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              Najlepszy program do poznania sympatii DZIAL
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>Tytuł</span>
+              <br /> Mama z sąsiedztwa czeka abyś u niej zagościł gdy męża nie
+              ma w domu!
+            </a>
+          </div>
+        </a>
+        <a href="https://redirecting3.eu/p/C4kJ/RbgM/h4ND" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              PROGRAM PO PROSTU FLIRT
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>ads12</span>
+              <br /> Umów się na herbatę albo od razu zerżnij napaloną siostrę
+              przyrodnią w wannie!
+            </a>
+          </div>
+        </a>
+        <a href="https://redirecting3.eu/p/C4kJ/P4mQ/qncM" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              COUNT EMPIRE GAME
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>Tytuł</span>
+              <br /> Znajdź napaloną mamuśkę w okolicy i wypieprz ją teraz!!!
+            </a>
+          </div>
+        </a>
+        <a href="https://redirecting3.eu/p/C4kJ/aLHC/WkhL" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              Program DATEST
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>Tytuł</span>
+              <br /> Zerżnij albo daj się zerżnąć!
+            </a>
+          </div>
+        </a>
+        <a href="https://leadmy.pl/p/C4kJ/jcJX/dbm8" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              Najbardziej polecany program!
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>Tytuł</span>
+              <br /> Niech to, że przychodzisz do mnie gdy on wychodzi zostanie
+              naszą słodką tajemnicą!
+            </a>
+          </div>
+        </a>
+        <a href="https://redirecting3.eu/p/C4kJ/UQ4u/poAC" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              Program DATES DZIAL
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>Tytuł</span>
+              <br /> Niech to, że przychodzisz do mnie gdy on wychodzi zostanie
+              naszą słodką tajemnicą!
+            </a>
+          </div>
+        </a>
+        <div style={{ background: "black", width: "80%", height: "1px" }}></div>
+        <div style={{ background: "black", width: "80%", height: "1px" }}></div>
+        <div style={{ background: "black", width: "80%", height: "1px" }}></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
