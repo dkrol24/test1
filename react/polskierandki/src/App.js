f547559e23ff08df89f12bc42d60010c0aa6e9cf
dkrol24 <krold2191@gmail.com>
2023-04-11 13:00:38 +0200

    .

---
+++ react/polskierandki/src/App.js
@@ -0,0 +1,85 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chcesz poznać kogoś i umówić się na randkę?</h1>
+      <h1>Zobacz strony na których osoby czekają właśnie na Ciebie!</h1>
+      <h1>
+        Najedź myszką na jedną z nich i przeczytaj czy pasuje do Twoich upodobań
+      </h1>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          width: "100%",
+          height: "45vh",
+          background: "pink",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{ background: "black", width: "100%", height: "1px" }}
+        ></div>
+        <div
+          style={{ background: "black", width: "100%", height: "1px" }}
+        ></div>
+        <div
+          style={{ background: "black", width: "100%", height: "1px" }}
+        ></div>
+        <a href="https://redirecting5.eu/p/C4kJ/MPCK/1DAT" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              MILFFINDER
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
+              <br /> Mama z sąsiedztwa czeka abyś u niej zagościł gdy męża nie
+              ma w domu!
+            </a>
+          </div>
+        </a>
+        <a href="https://redirecting5.eu/p/C4kJ/dxn0/uiHP" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              PINEROTICA
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
+              <br /> Mama z sąsiedztwa czeka abyś u niej zagościł gdy męża nie
+              ma w domu!
+            </a>
+          </div>
+        </a>
+        <a href="https://redirecting5.eu/p/C4kJ/3zNg/9TIC" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              WIECEJNIZFLIRT
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
+              <br /> Umów się na herbatę albo od razu zerżnij napaloną siostrę
+              przyrodnią w wannie!
+            </a>
+          </div>
+        </a>
+        <a href="	https://redirecting5.eu/p/C4kJ/oBl1/yq58" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              MY DIRTY HOBBY
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
+              <br /> Znajdź napaloną mamuśkę w okolicy i wypieprz ją teraz!!!
+            </a>
+          </div>
+        </a>
+        <div style={{ background: "black", width: "80%", height: "1px" }}></div>
+        <div style={{ background: "black", width: "80%", height: "1px" }}></div>
+        <div style={{ background: "black", width: "80%", height: "1px" }}></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
